8b51cd20f87c9b9a2c3d17323c16e5ec7646e54d
KayserV <hans.buddenberg@me.com>
2014-08-13 19:11:05 -0400

    Test

---
+++ CheckedListBox/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckedListBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string[] MyCheckList)
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Checks = MyCheckList;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Checks(string[] values, string name)
+        {
+            ViewBag.Name = name;
+            return View(values);
+        }
+    }
+}
---
+++ CheckedListBox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CheckedListBox.MvcApplication" Language="C#" %>
